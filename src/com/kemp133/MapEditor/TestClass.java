92319cbd4ada4fbf8b8dce7b29fdf4ce59250478
Johnathon Kemp <kemp133@googlemail.com>
2020-02-21 19:49:02 +0000

    Adding MenuItems to the current Menu headers

--- src/com/kemp133/MapEditor/TestClass.java
+++ src/com/kemp133/MapEditor/TestClass.java
@@ -3,12 +3,16 @@ package com.kemp133.MapEditor;
 import com.kemp133.MapEditor.Lang.UILabelLanguageResources;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.util.ResourceBundle;
@@ -30,19 +34,42 @@ public class TestClass extends Application {
 
 	@Override
 	public void start(Stage stage) {
-		final Menu fileMenu    = new Menu(UIText.getString("fileMenu"));
+		//region Parent Menu Items
+		final Menu fileMenu = new Menu(UIText.getString("fileMenu"));
 		final Menu optionMenu = new Menu(UIText.getString("optionMenu"));
-		final Menu helpMenu    = new Menu(UIText.getString("helpMenu"));
+		final Menu helpMenu = new Menu(UIText.getString("helpMenu"));
+		//endregion
+
+		//region File Menu Items
+		final MenuItem fileMenu_new  = new MenuItem(UIText.getString("fileMenu_new"));
+		final MenuItem fileMenu_open = new MenuItem(UIText.getString("fileMenu_open"));
+		final MenuItem fileMenu_save = new MenuItem(UIText.getString("fileMenu_save"));
+		//endregion
+
+		//region Option Menu Items
+		final MenuItem optionMenu_settings = new MenuItem(UIText.getString("optionMenu_settings"));
+		//endregion
+
+		//region Help Menu Items
+		final MenuItem helpMenu_help = new MenuItem(UIText.getString("helpMenu_help"));
+		//endregion
 
 		MenuBar menubar = new MenuBar();
+
+		fileMenu.getItems().addAll(fileMenu_new, fileMenu_open, fileMenu_save);
+		optionMenu.getItems().addAll(optionMenu_settings);
+		helpMenu.getItems().addAll(helpMenu_help);
+
 		menubar.getMenus().addAll(fileMenu, optionMenu, helpMenu);
 
-		VBox test = new VBox(menubar);
-		GridPane pane = new GridPane();
-		pane.setHgap(150);
-		pane.setVgap(200);
+		BorderPane borderPane = new BorderPane();
+		borderPane.setTop(menubar);
 
-		Scene scene = new Scene(test, 800, 600);
+		ScrollPane rightMenu = new ScrollPane();
+		rightMenu.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
+		borderPane.setRight(rightMenu);
+
+		Scene scene = new Scene(borderPane, 800, 600);
 		stage.setTitle("Map Editor");
 		stage.setResizable(false);
 		stage.setMaxHeight(600);
